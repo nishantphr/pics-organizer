5735b8db29612dc3e62f743cdae8d008ddcee77e
nishantphr <54103742+nishantphr@users.noreply.github.com>
2021-04-10 20:48:32 +0530

    Update pics-organizer.py
    overhaul

--- pics-organizer.py
+++ pics-organizer.py
@@ -4,143 +4,163 @@ from datetime import datetime
 from collections import defaultdict
 import exifread
 import piexif
+import hachoir.parser
+import hachoir.metadata
 
 
-def is_video_ext(ext):
-    return ext in ["3gp", "avi", "mp4", "mpg", "mts"]
+class FileInfo:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.ext = os.path.splitext(filepath)[1][1:].lower()
 
+        if self.is_pic_or_video():
+            print("*" * 20)
+            print("Reading", self.filepath)
+            self.size = os.path.getsize(self.filepath)
+            self.timestamp = self.get_timestamp()
+            print("*" * 20)
 
-def is_pic_ext(ext):
-    return ext in ["jpg", "jpeg", "png"]
+    def is_video_file(self):
+        return self.ext in ["3gp", "avi", "mp4", "mpg", "mts", "mov"]
 
+    def is_pic_file(self):
+        return self.is_jpg_file() or self.ext in ["png"]
 
-def get_ext_lower(file):
-    return os.path.splitext(file)[1][1:].lower()
+    def is_jpg_file(self):
+        return self.ext in ["jpg", "jpeg"]
 
+    def is_pic_or_video(self):
+        return self.is_pic_file() or self.is_video_file()
 
-def get_file_modified_time(file):
-    return datetime.fromtimestamp(os.path.getmtime(file))
+    def get_timestamp(self):
+        modified_time = datetime.fromtimestamp(os.path.getmtime(self.filepath))
+        metadata_time = None
+        invalid_time = datetime.strptime("1980-01-01", "%Y-%m-%d")
 
+        if self.is_jpg_file():
+            try:
+                with open(self.filepath, "rb") as f:
+                    tags = exifread.process_file(f, details=False, stop_tag="DateTimeOriginal")
+                    dt_str = str(tags["EXIF DateTimeOriginal"])
 
-def get_file_modified_time_size(file):
-    return os.path.getsize(file), get_file_modified_time(file)
+                metadata_time = datetime.strptime(dt_str, "%Y:%m:%d %H:%M:%S")
+            except Exception as e:
+                print("ERROR: %s" % e)
 
+        elif self.is_video_file() or self.is_pic_file():
+            try:  # this try for when the createParser below return None and hence "with" clause fails
+                with hachoir.parser.createParser(self.filepath) as parser:
+                    metadata = hachoir.metadata.extractMetadata(parser)
+                    try:
+                        metadata_time = metadata.get("creation_date", default=None)
+                    except Exception as e:
+                        print("ERROR: Could not fine creation_date from following metadata:")
+                        print(metadata)
+                        print("Original error:\n%s" % e)
+            except Exception as e:
+                print("ERROR: \n%s" % e)
 
-def get_jpg_date_time(file):
-    dt = datetime.today()
-    try:
-        with open(file, "rb") as f:
-            tags = exifread.process_file(f, details=False, stop_tag="DateTimeOriginal")
-            dt_str = str(tags["EXIF DateTimeOriginal"])
+        print("metadata time = %s" % metadata_time)
+        print("file modified time = %s" % modified_time)
 
-        dt = datetime.strptime(dt_str, "%Y:%m:%d %H:%M:%S")
-    except:
-        pass
-    finally:
+        # date of metadata time and modified time are same then use modified time as metadata time is always UTC.
+        if metadata_time and metadata_time.date() != modified_time.date() and invalid_time < metadata_time < modified_time:
+            dt = metadata_time
+            print("using metadata time")
+        else:
+            dt = modified_time
+            print("using file modified time")
+
+        dt = dt.replace(microsecond=0)  # precision till seconds only.
         return dt
 
 
-def get_file_time(file):
-    ext = get_ext_lower(file)
-    if "jpg" in ext or "jpeg" in ext:
-        dt_jpg = get_jpg_date_time(file)
-    else:
-        dt_jpg = datetime.today()
+class PicsOrganizer:
+    def __init__(self, in_dir, out_dir):
+        self.in_dir = in_dir
+        self.out_dir = out_dir
 
-    dtf = get_file_modified_time(file)
+    def scan_input_dir(self):
+        print("Scanning input dir ", self.in_dir)
+        src_dest_tuple_list = []
+        dest_set = set()
+        for dir_name, sub_dirs_list, files_list in os.walk(self.in_dir):
+            sorted_files_list = sorted(files_list)
+            for src_file_path in sorted_files_list:
+                src_file_path = os.path.join(dir_name, src_file_path)
+                info = FileInfo(src_file_path)
+                if info.is_pic_or_video():
+                    # WARNING: A change here also demands change in parsing below (parts = dest.split("-"))
+                    dest_file_name = info.timestamp.strftime("%Y-%m-%d_%H%M%S")
+                    org_dest_file = dest_file_name
 
-    today = datetime.today()
+                    i = 0
+                    while dest_file_name in dest_set:
+                        i += 1
+                        suffix = "D%03d" % i
+                        dest_file_name = "%s_%s" % (org_dest_file, suffix)
 
-    # Use the older datetime of two. If both dates are same (and different time) then prefer dtjpg
-    dt = dt_jpg if dt_jpg < dtf or dt_jpg.date() == dtf.date() else dtf
-    #dt = dt_jpg if dt_jpg.date() != today.date() else dtf
+                    if i > 0:
+                        print("Duplicate time found '%s' for '%s'" % (org_dest_file, src_file_path))
 
-    dt = dt.replace(microsecond=0) # precision till seconds only.
+                    dest_set.add(dest_file_name)
+                    date_dir = dest_file_name.split("-")[0]  # folders by years
+                    dest_file_path = os.path.join(self.out_dir, date_dir, "%s.%s" % (dest_file_name, info.ext))
+                    src_dest_tuple_list.append((src_file_path, dest_file_path))
 
-    if dt.date() == today.date():
-        #raise RuntimeError("Error getting file time for %s, dtjpg=%s, dtf=%s" % (file, str(dtjpg), str(dtf)))
-        return None
+        return src_dest_tuple_list
 
-    return dt
+    @staticmethod
+    def create_dest_dirs(src_dest_tuple_list):
+        print("Creating destination dirs...")
+        dirs_to_create = set()
+        for _, dest in src_dest_tuple_list:
+            dirs_to_create.add(os.path.dirname(dest))
+
+        for d in dirs_to_create:
+            os.makedirs(d)
+
+    @staticmethod
+    def move_files(src_dest_tuple_list):
+        print("Moving files...")
+        for src, dest in src_dest_tuple_list:
+            print(src, "->", dest)
+            if os.path.exists(dest):
+                raise RuntimeError("Dest file %s already exists" % dest)
+            shutil.move(src, dest)
+
+    def run(self):
+        files = os.listdir(self.out_dir)
+        if files:
+            raise RuntimeError("out_dir %s is not empty" % self.out_dir)
+
+        src_dest_tuple_list = self.scan_input_dir()
+        self.create_dest_dirs(src_dest_tuple_list)
+        self.move_files(src_dest_tuple_list)
+        remove_empty_dirs(self.in_dir)
+
+
+def remove_empty_dirs(dir_path):
+    print("Removing empty dirs in %s" % dir_path)
+    # walking the tree bottom - up is essential, rmdir() doesn’t allow
+    # deleting a non empty directory
+    # assuming there are no symbolic links.
+    for root, dirs, files in os.walk(dir_path, topdown=False):
+        for name in dirs:
+            d = os.path.join(root, name)
+            if not os.listdir(d):
+                os.rmdir(d)
 
 
 def get_date_folder_month(month):
-    if month in [1,2,3,4]:
+    if month in [1, 2, 3, 4]:
         return "A"
-    if month in [5,6,7,8]:
+    if month in [5 ,6 ,7, 8]:
         return "B"
-    if month in [9,10,11,12]:
+    if month in [9, 10, 11, 12]:
         return "C"
 
     raise RuntimeError("getDateFolderMonth error for month %d" % month)
-
-
-def get_src_dest_tuple_list(indir, outdir):
-    print("Reading input files...")
-    src_dest_tuple_list = []
-    dest_to_src_map = {}
-    dest_set = set()
-    for dir_name, sub_dirs_list, files_list in os.walk(indir):
-        sorted_files_list = sorted(files_list)
-        for src_file in sorted_files_list:
-            src_file = os.path.join(dir_name, src_file)
-            print("Reading", src_file, "-")
-
-            dt = get_file_time(src_file)
-            if dt == None:
-                print("IGNORED..")
-                continue
-
-            # WARNING: A change here also demands change in parsing below (parts = dest.split("-"))
-            dest_file = dt.strftime("%Y-%m-%d_%H-%M-%S")
-            print(dest_file)
-
-            org_dest_file = dest_file
-            i = 0
-            suffix = ""
-            while dest_file in dest_set:
-                i += 1
-                suffix = "D%03d" % i
-                dest_file = "%s_%s" % (org_dest_file, suffix)
-
-            if i > 0:
-                print("Duplicate time found '%s' for '%s' and '%s', added suffix %s" % (org_dest_file, src_file, dest_to_src_map[org_dest_file], suffix))
-
-            dest_set.add(dest_file)
-            dest_to_src_map[dest_file] = src_file
-            src_dest_tuple_list.append((src_file, dest_file))
-
-    src_dest_tuple_list2 = []
-    for src, dest in src_dest_tuple_list:
-        parts = dest.split("-")
-        # dateFolder = "%s_%s" % (parts[0], getDateFolderMonth(int(parts[1])))
-        date_folder = parts[0]
-        ext = get_ext_lower(src)
-        # fileType = "pics" if is_pic_ext(ext) else "vids" if is_video_ext(ext) else "other"
-
-        # src_dest_tuple_list2.append((src, os.path.join(outdir, fileType, date_folder, "%s.%s" % (dest, ext))))
-        src_dest_tuple_list2.append((src, os.path.join(outdir, date_folder, "%s.%s" % (dest, ext))))
-
-    return src_dest_tuple_list2
-
-
-def create_dest_dirs(src_dest_tuple_list):
-    print("Creating destination dirs...")
-    dirs_to_create = set()
-    for _, dest in src_dest_tuple_list:
-        dirs_to_create.add(os.path.dirname(dest))
-
-    for d in dirs_to_create:
-        os.makedirs(d)
-
-
-def move_files(src_dest_tuple_list):
-    print("Moving files...")
-    for src, dest in src_dest_tuple_list:
-        print(src, "->", dest)
-        if os.path.exists(dest):
-            raise RuntimeError("Dest file %s already exists" % dest)
-        shutil.move(src, dest)
 
 
 def move_files_to_dir(files, dest_dir):
@@ -161,21 +181,6 @@ def copy_files_to_dir(files, destDir):
         if os.path.exists(dest):
             raise RuntimeError("Dest file %s already exists" % dest)
         shutil.copy(src, dest)
-
-
-def organize_files(indir, outdir):
-    indir = os.path.normpath(indir)
-    outdir = os.path.normpath(outdir)
-
-    files = os.listdir(outdir)
-    if files:
-        raise RuntimeError("outdir %s is not empty" % outdir)
-
-    src_dest_tuple_list = get_src_dest_tuple_list(indir, outdir)
-
-    create_dest_dirs(src_dest_tuple_list)
-
-    move_files(src_dest_tuple_list)
 
 
 def remove_duplicates():
@@ -260,7 +265,14 @@ def set_file_mod_time_from_jpeg_time(file):
     os.utime(file, (timestamp, timestamp))
 
 
+def main():
+    IN_DIR = "D:\_00src"
+    OUT_DIR = "D:\_00dest"
+    IN_DIR = "D:/pics-and-vidoes-src"
+    OUT_DIR = "D:/pics-and-vidoes-dest"
+    PicsOrganizer(IN_DIR, OUT_DIR).run()
+
+
 if __name__ == "__main__":
-    IN_DIR = "D:\pics_and_vids"
-    OUT_DIR = "D:\og"
-    organize_files(IN_DIR, OUT_DIR)
+    #test()
+    main()
